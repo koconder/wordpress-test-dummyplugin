ceac7c6fae4ff0325f40de10201486dccbf2a9eb
Vincent K <koconder@gmail.com>
2019-01-24 14:49:12 +1100

    Fixes

--- boilerplate-dummy-plugin/boilerplate-dummy-plugin.php
+++ boilerplate-dummy-plugin/boilerplate-dummy-plugin.php
@@ -8,9 +8,9 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              https://github.com/koconder/wordpress-test-dummyplugin
- * @since             1.0.0
- * @package           Boilerplate_Dummy_Plugin
+ * @link    https://github.com/koconder/wordpress-test-dummyplugin
+ * @since   1.0.0
+ * @package Boilerplate_Dummy_Plugin
  *
  * @wordpress-plugin
  * Plugin Name:       Boilerplate Dummy Plugin
@@ -26,8 +26,8 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (! defined('WPINC') ) {
+    die;
 }
 
 /**
@@ -35,34 +35,36 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define('PLUGIN_NAME_VERSION', '1.0.0');
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-plugin-name-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
+function activate_plugin_name() 
+{
+    include_once plugin_dir_path(__FILE__) . 'includes/class-plugin-name-activator.php';
+    Plugin_Name_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-plugin-name-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+function deactivate_plugin_name() 
+{
+    include_once plugin_dir_path(__FILE__) . 'includes/class-plugin-name-deactivator.php';
+    Plugin_Name_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook(__FILE__, 'activate_plugin_name');
+register_deactivation_hook(__FILE__, 'deactivate_plugin_name');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path(__FILE__) . 'includes/class-plugin-name.php';
 
 /**
  * Begins execution of the plugin.
@@ -71,12 +73,13 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
- * @since    1.0.0
+ * @since 1.0.0
  */
-function run_plugin_name() {
+function run_plugin_name() 
+{
 
-	$plugin = new Plugin_Name();
-	$plugin->run();
+    $plugin = new Plugin_Name();
+    $plugin->run();
 
 }
 run_plugin_name();
